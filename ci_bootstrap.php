6b39d1babfc3e293b8007dfb5206f9aa3a7d04d4
LewisWBPD <lewis@bigpropertydata.com>
2018-04-17 16:56:56 +0100

    3.1.6 Compatibility
    * 3.1.6 Compatibility

--- ci_bootstrap.php
+++ ci_bootstrap.php
@@ -53,46 +53,81 @@ function ci_bootstrap($kernel, $override_controller_class = false, $load_fake_co
      * @var string
      *
      */
-    define('CI_VERSION', '2.1.0');
+	define('CI_VERSION', '3.1.6');
 
-    /**
-     * CodeIgniter Branch (Core = TRUE, Reactor = FALSE)
-     *
-     * @var boolean
-     *
-     */
-    define('CI_CORE', FALSE);
+	/*
+	 * ------------------------------------------------------
+	 *  Load the framework constants
+	 * ------------------------------------------------------
+	 */
+	if (file_exists(APPPATH.'config/'.ENVIRONMENT.'/constants.php'))
+	{
+		require_once(APPPATH.'config/'.ENVIRONMENT.'/constants.php');
+	}
+
+	if (file_exists(APPPATH.'config/constants.php'))
+	{
+		require_once(APPPATH.'config/constants.php');
+	}
 
     /*
      * ------------------------------------------------------
      *  Load the global functions
      * ------------------------------------------------------
      */
-    require(BASEPATH . 'core/Common.php');
+	require_once(BASEPATH.'core/Common.php');
+
 
     /*
      * ------------------------------------------------------
-     *  Load the framework constants
+ 	 * Security procedures
      * ------------------------------------------------------
      */
-    if (defined('ENVIRONMENT') AND file_exists(APPPATH . 'config/' . ENVIRONMENT . '/constants.php')) {
-        require(APPPATH . 'config/' . ENVIRONMENT . '/constants.php');
-    } else {
-        require(APPPATH . 'config/constants.php');
-    }
 
-    /*
-     * ------------------------------------------------------
-     *  Define a custom error handler so we can log PHP errors
-     * ------------------------------------------------------
-     */
-    set_error_handler('_exception_handler');
+	if ( ! is_php('5.4'))
+	{
+		ini_set('magic_quotes_runtime', 0);
 
-    if (!is_php('5.3')) {
-        @set_magic_quotes_runtime(0); // Kill magic quotes
-    }
+		if ((bool) ini_get('register_globals'))
+		{
+			$_protected = array(
+				'_SERVER',
+				'_GET',
+				'_POST',
+				'_FILES',
+				'_REQUEST',
+				'_SESSION',
+				'_ENV',
+				'_COOKIE',
+				'GLOBALS',
+				'HTTP_RAW_POST_DATA',
+				'system_path',
+				'application_folder',
+				'view_folder',
+				'_protected',
+				'_registered'
+			);
 
-    /*
+			$_registered = ini_get('variables_order');
+			foreach (array('E' => '_ENV', 'G' => '_GET', 'P' => '_POST', 'C' => '_COOKIE', 'S' => '_SERVER') as $key => $superglobal)
+			{
+				if (strpos($_registered, $key) === FALSE)
+				{
+					continue;
+				}
+
+				foreach (array_keys($$superglobal) as $var)
+				{
+					if (isset($GLOBALS[$var]) && ! in_array($var, $_protected, TRUE))
+					{
+						$GLOBALS[$var] = NULL;
+					}
+				}
+			}
+		}
+	}
+
+	/*
      * ------------------------------------------------------
      *  Set the subclass_prefix
      * ------------------------------------------------------
@@ -101,23 +136,47 @@ function ci_bootstrap($kernel, $override_controller_class = false, $load_fake_co
      * The subclass prefix allows CI to know if a core class is
      * being extended via a library in the local application
      * "libraries" folder. Since CI allows config items to be
-     * overriden via data set in the main index. php file,
+     * overridden via data set in the main index.php file,
      * before proceeding we need to know if a subclass_prefix
      * override exists.  If so, we will set this value now,
      * before any classes are loaded
      * Note: Since the config file data is cached it doesn't
      * hurt to load it here.
      */
-    if (isset($assign_to_config['subclass_prefix']) AND $assign_to_config['subclass_prefix'] != '') {
+	if ( ! empty($assign_to_config['subclass_prefix']))
+	{
         get_config(array('subclass_prefix' => $assign_to_config['subclass_prefix']));
     }
+
+	/*
+	 * ------------------------------------------------------
+	 *  Should we use a Composer autoloader?
+	 * ------------------------------------------------------
+	 */
+	if ($composer_autoload = config_item('composer_autoload'))
+	{
+		if ($composer_autoload === TRUE)
+		{
+			file_exists(APPPATH.'vendor/autoload.php')
+				? require_once(APPPATH.'vendor/autoload.php')
+				: log_message('error', '$config[\'composer_autoload\'] is set to TRUE but '.APPPATH.'vendor/autoload.php was not found.');
+		}
+		elseif (file_exists($composer_autoload))
+		{
+			require_once($composer_autoload);
+		}
+		else
+		{
+			log_message('error', 'Could not find the specified $config[\'composer_autoload\'] path: '.$composer_autoload);
+		}
+	}
 
     /*
      * ------------------------------------------------------
      *  Start the timer... tick tock tick tock...
      * ------------------------------------------------------
      */
-    $BM = load_class('Benchmark', 'core');
+	$BM = load_class('Benchmark', 'core');
     $BM->mark('total_execution_time_start');
     $BM->mark('loading_time:_base_classes_start');
 
@@ -126,7 +185,7 @@ function ci_bootstrap($kernel, $override_controller_class = false, $load_fake_co
      *  Instantiate the hooks class
      * ------------------------------------------------------
      */
-    $EXT = load_class('Hooks', 'core');
+	$EXT = load_class('Hooks', 'core');
 
     /*
      * ------------------------------------------------------
@@ -139,13 +198,84 @@ function ci_bootstrap($kernel, $override_controller_class = false, $load_fake_co
      * ------------------------------------------------------
      *  Instantiate the config class
      * ------------------------------------------------------
+	 *
+	 * Note: It is important that Config is loaded first as
+	 * most other classes depend on it either directly or by
+	 * depending on another class that uses it.
+	 *
      */
-    $CFG = load_class('Config', 'core');
+	$CFG = load_class('Config', 'core');
     
     // Do we have any manually set config items in the index.php file?
-    if (isset($assign_to_config)) {
-        $CFG->_assign_to_config($assign_to_config);
+	if (isset($assign_to_config) && is_array($assign_to_config))
+	{
+		foreach ($assign_to_config as $key => $value)
+		{
+			$CFG->set_item($key, $value);
+		}
     }
+
+/*
+ * ------------------------------------------------------
+ * Important charset-related stuff
+ * ------------------------------------------------------
+ *
+ * Configure mbstring and/or iconv if they are enabled
+ * and set MB_ENABLED and ICONV_ENABLED constants, so
+ * that we don't repeatedly do extension_loaded() or
+ * function_exists() calls.
+ *
+ * Note: UTF-8 class depends on this. It used to be done
+ * in it's constructor, but it's _not_ class-specific.
+ *
+ */
+	$charset = strtoupper(config_item('charset'));
+	ini_set('default_charset', $charset);
+
+	if (extension_loaded('mbstring'))
+	{
+		define('MB_ENABLED', TRUE);
+		// mbstring.internal_encoding is deprecated starting with PHP 5.6
+		// and it's usage triggers E_DEPRECATED messages.
+		@ini_set('mbstring.internal_encoding', $charset);
+		// This is required for mb_convert_encoding() to strip invalid characters.
+		// That's utilized by CI_Utf8, but it's also done for consistency with iconv.
+		mb_substitute_character('none');
+	}
+	else
+	{
+		define('MB_ENABLED', FALSE);
+	}
+
+	// There's an ICONV_IMPL constant, but the PHP manual says that using
+	// iconv's predefined constants is "strongly discouraged".
+	if (extension_loaded('iconv'))
+	{
+		define('ICONV_ENABLED', TRUE);
+		// iconv.internal_encoding is deprecated starting with PHP 5.6
+		// and it's usage triggers E_DEPRECATED messages.
+		@ini_set('iconv.internal_encoding', $charset);
+	}
+	else
+	{
+		define('ICONV_ENABLED', FALSE);
+	}
+
+	if (is_php('5.6'))
+	{
+		ini_set('php.internal_encoding', $charset);
+	}
+
+	/*
+	 * ------------------------------------------------------
+	 *  Load compatibility features
+	 * ------------------------------------------------------
+	 */
+
+	require_once(BASEPATH.'core/compat/mbstring.php');
+	require_once(BASEPATH.'core/compat/hash.php');
+	require_once(BASEPATH.'core/compat/password.php');
+	require_once(BASEPATH.'core/compat/standard.php');
 
     /*
      * ------------------------------------------------------
@@ -393,7 +523,7 @@ function ci_bootstrap($kernel, $override_controller_class = false, $load_fake_co
             // Any URI segments present (besides the class/function) will be passed to the method for convenience
             call_user_func_array(array(&$CI, $method), array_slice($URI->rsegments, 2));
         }
-    
+
     /** MODIFICATION FOR SYMFONY (add) */
     }
     /** END */
